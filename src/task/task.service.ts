8089b808108db05a20afa7295ae08c90b096765c
Carlos Aurelio Briceño Gomez <aurel.gomez@outlook.com>
2023-05-13 01:16:59 -0500

    before develop front

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,18 +1,15 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { timeout, lastValueFrom } from 'rxjs';
 
 import { Task } from '../db/models/task.entity';
 import { CreateTaskDto, UpdateTaskDto } from './task.dto';
-import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class TaskService {
   constructor(
     @InjectRepository(Task)
     private readonly taskRepository: Repository<Task>,
-    @Inject('MICRO_CHANNEL') private client: ClientProxy,
   ) {}
 
   async findAll(userId: number): Promise<Task[]> {
@@ -23,15 +20,6 @@ export class TaskService {
         },
       },
     });
-  }
-
-  validateToken(): Promise<number> {
-    const pattern = { cmd: 'validate-token' };
-    const payload =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiY3JlYXRlZEF0IjoiMjAyMy0wNS0xM1QwNTo1Mzo0NC41MTlaIiwidXBkYXRlZEF0IjoiMjAyMy0wNS0xM1QwNTo1Mzo0NC41MTlaIiwiZGVsZXRlZEF0IjpudWxsLCJ1c2VybmFtZSI6IiIsIm1haWwiOiJ5b0BtbWUuY29tIiwiaWF0IjoxNjgzOTM5NDE4LCJleHAiOjE2ODkxMjM0MTh9.rAmY7_eqtBmzIDKYqRT6OmIkBZJCl3sV9hDbaYLccn';
-    return lastValueFrom(
-      this.client.send(pattern, payload).pipe(timeout(5000)),
-    );
   }
 
   async create(task: CreateTaskDto): Promise<Task> {
